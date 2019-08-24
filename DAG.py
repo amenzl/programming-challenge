5debbee8559bb91754231deda8c66efbe9a8684c
jmatzke <ge69rec@tum.de>
2019-08-24 10:54:23 -0500

    restructured

--- DAG.py
+++ DAG.py
@@ -1,33 +1,48 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Sat Aug 24 07:38:37 2019
+Created on Sat Aug 24, 2019
 
 @author: anna
 """
 
-#import networkx as nx
-#networkx creates graphs, functions include Graph(), add_nodes, add_edges
+class DAG(object):
 
-class DAG(nodes, edges):
-
-    def __init__(self, initial_nodes):
-        self.total_nodes = initial_nodes
-    
-    def add_node(nodes):
-
-    
-    def add_edges(edges):
-
-
-class node(name, number, neighbor1, neighbor2):
-    
-    def __init__(self):
-        self.neighbor_one = None
-        self.neighbor_two = None
-
-class edges()
-
-##To draw the graph
-nx.draw(G)
+    def __init__(self, number_nodes, number_edges):
+        self.total_nodes = number_nodes #create member variable to hold number of nodes
+        self.total_edges = number_edges #create member variable to hold number of edges
+        self.node_list =[] #create member list of all node objects
         
+        #create nodes
+        for i in range(self.total_nodes):
+            self.add_node(i)
+
+        assert (node_list.len() == self.total_nodes) #check all nodes created
+            
+        #create edges
+        for j in range(self.total_edges):
+            #TODO: need to ensure this is done acyclically; clever ordering/topology needed
+            self.add_edge()
+    
+    def add_node(self, name):
+        add_node.append(node(name))
+    
+    def add_edge(self, node_one, node_two):
+    #TODO:
+        node_one.set_neighbor_upstream(node_two.name)
+        node_two.set_neighbor_downstream(node_one.name)
+
+
+class node(object):
+    
+    def __init__(self, name):
+        self.name = name
+        self.neighbor_upstream = None
+        self.neighbor_downstream = None
+
+    def set_neighbor_upstream(self, neighbor_name):
+        self.neighbor_upstream = neighbor_name
+
+    def set_neighbor_downstream(self, neighbor_name):
+        self.neighbor_downstream = neighbor_name
+
---
+++ main.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 24, 2019
+    
+@author: anna
+"""
+
+from . import DAG
+
+nodes = 20
+edges = 20
+
+g = DAG(nodes, edges)
