cdf052a9d31abf9c0d5070c7074bdecfd47e3f7a
jmatzke <ge69rec@tum.de>
2019-08-25 14:21:48 -0500

    added timing

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ from DAG import DAG
 import copy as c
 import pandas as pd
 import random as r
+import time
 
 nodes = 20
 edges = 20
@@ -42,21 +43,28 @@ df=df.rename(columns={target: "Target"})
 export_csv = df.to_csv(r'first_rand_df.csv', index = None, header=True)
 
 for j in range(100):
+    loop_time = time.time()
     print(j)
     list_data=[]
+    graph_time = time.time()
     for i in range(1000):
         list_data.append(c.deepcopy(g.return_node_value_dict()))
 #print(list_data)
 #print(pd.DataFrame(list_data))
-    
+    print("took %f s to run graph" % time.time()-graph_time)
+    df_time = time.time()
     df=pd.DataFrame.from_dict(list_data)
     #Select a random target
     nodes=sorted(g.node_dict.keys())
     target=r.randint(0, len(nodes))
     df=df.rename(columns={target: "Target"})
+    print("took %f s to create df" % time.time()-df_time)
 
+    export_time = time.time()
     export_csv = df.to_csv(r'rand_df'+str(j)+'.csv', index = None, header=True)
+    print("took %f to export" % time.time()-export_time)
 
+    print("j=%i took %f s total" % (j, time.time()-loop_time))
     
 
     
