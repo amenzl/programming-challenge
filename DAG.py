d281a3c78eaf958d5096726a3cc0043c3019ca44
Anna <anna@WALTERLAB-AnnaM.local>
2019-08-24 22:18:22 -0600

    added parents

--- DAG.py
+++ DAG.py
@@ -6,7 +6,6 @@ Created on Sat Aug 24, 2019
 @author: anna
 """
 import random as r
-import numpy as np
 
 class DAG(object):
 
@@ -17,6 +16,7 @@ class DAG(object):
         self.actual_edges = 0
         
         assert (number_edges >= (number_nodes-1)) #check this is a possible graph
+        assert (number_edges<=(number_nodes*(number_nodes- 1))/2)
         
         #Create a list with a randomly generated order number for nodes
         node_order_list = r.sample(range(number_nodes), number_nodes)
@@ -41,9 +41,6 @@ class DAG(object):
             dep_node_pot = list(range(node_order+1, self.total_nodes))
             
             ## Randomly pick dependent nodes from list of potential dep nodes
-            print('name of node: %i' %node_order)
-            print('number of potential nodes: %i' % len(dep_node_pot))
-            print('number of dependent nodes: %i' % num_dep_nodes)
             assert (len(dep_node_pot) >= num_dep_nodes)
             
             dep_nodes = r.sample(dep_node_pot, num_dep_nodes)
@@ -55,7 +52,7 @@ class DAG(object):
                 for j in range(num_dep_nodes):
                     self.node_dict[node_order].set_dep_nodes(dep_nodes[j])
                 actual_edges += num_dep_nodes
-                print('Number of edges %i' %actual_edges)
+#                print('Number of edges %i' %actual_edges)
                 
                     
             else:
@@ -70,50 +67,42 @@ class DAG(object):
                 #check which nodes have less dependents than potential dependents
                 if len(self.node_dict[key].dep_node) < number_nodes-(key+1):
                     #Add all those nodes to a list
-                    print("open nodes %i" %key)
                     open_nodes.append(key)
-#                    print(open_nodes)
                     
                      # Pick a random entry from that list
-            print("here is the open node list")
-            print(open_nodes)
             sel_node_index = r.randint(0, len(open_nodes)-1)
-            print("here is the selected open node index %i" %sel_node_index)
             sel_node=open_nodes[sel_node_index]
-            print(self.node_dict[key].dep_node(sel_node))
-            print("here is the selected open node %i" %sel_node)
             dep_node_pot = list(range(sel_node+1, self.total_nodes))
-            print("here is the list of potential nodes")
-            print(dep_node_pot)
-            dep_node_pot_new=list(set(dep_node_pot) - set(self.node_dict[key].dep_node))
-            print("here is the list of new potential nodes")
-            print(dep_node_pot_new)
+            dep_node_pot_new=list(set(dep_node_pot) - set(self.node_dict[sel_node].dep_node))
             dep_nodes_new = r.sample(dep_node_pot_new, 1)
-            self.node_dict[key].set_dep_nodes(dep_nodes_new[0])
+            self.node_dict[sel_node].set_dep_nodes(dep_nodes_new[0])
             actual_edges+=1
-#            dep_node_pot_new=np.setdiff1d(np.asarray(dep_node_pot),np.asarray(self.node_dict[key].dep_node))
-            print("here is the new node")
-            print(dep_nodes_new)
-#            print("open nodes %i" %open_nodes[sel_node])
-                    
+        while number_edges<actual_edges:
+            pot_nodes=[]
+            for key in self.node_dict.keys():
+                if(len(self.node_dict[key].dep_node)>1):
+                    pot_nodes.append(key)
+            sel_node_index=r.randint(0, len(pot_nodes)-1)
+            sel_node=pot_nodes[sel_node_index]
             
-            
-#            and add a node
-            
-#            add a dependent
-#        if number_edge<= actual_edges:
-#                remove a dependent
-            
-        #create edges
-#        for j in range(self.total_edges):
-            #TODO: need to ensure this is done acyclically; clever ordering/topology needed
-#            self.add_edge()
+            del_node_index = r.randint(0, len(self.node_dict[sel_node].dep_node)-1)
+            self.node_dict[sel_node].dep_node.pop(del_node_index)
+            actual_edges-=1
+                
+        for key in self.node_dict.keys():
+            print('Name of node: %i' %key)
+            for node in self.node_dict.keys():
+                if key in self.node_dict[node].dep_node:
+                    print("Here are the dependent nodes of %i" %node)
+                    print(self.node_dict[node].dep_node)
+                    self.node_dict[key].find_parents(node)
+#                    print('Parents: %i' %self.node_dict[node])
     
     def add_node(self, name):
         self.node_dict.update({name:node(name)})
     
     def print_dep_nodes(self):
-        #ITerat over node dictionary
+        #Iterate over node dictionary
         for key in self.node_dict.keys():
             print('Name of node: %i' %key)
             if len(self.node_dict[key].dep_node)>0:
@@ -121,14 +110,15 @@ class DAG(object):
                     print('Dependent nodes:%i' %self.node_dict[key].dep_node[dep])
             else: 
                 print("No dependent nodes")
-#        print(node)
-        #get dependent nodes and print them
-    
-    
-#    def add_edge(self, node_one, node_two):
-#    #TODO:
-#        node_one.set_neighbor_upstream(node_two.name)
-#        node_two.set_neighbor_downstream(node_one.name)
+    def print_parents(self):
+        for key in self.node_dict.keys():
+            print('Name of node: %i' % key)
+            if(len(self.node_dict[key].parent)>0):
+                for parent in range(len(self.node_dict[key].parent)):
+                    print('Parents: %i' %self.node_dict[key].parent[parent])
+            else: 
+                print("No parents")
+                
 
 
 class node(object):
@@ -136,17 +126,21 @@ class node(object):
     def __init__(self, name):
         self.name = name
         self.dep_node = []
-        self.parent=0
+        self.parent=[]
+        self.node_value=None
 
     def set_dep_nodes(self, dep_name):
         self.dep_node.append(dep_name)
+    def find_parents(self, parent):
+        self.parent.append(parent)
 
     def set_neighbor_downstream(self, neighbor_name):
         self.neighbor_downstream = neighbor_name
 
 nodes = 5
-edges = 10
+edges = 9
 
 
 g = DAG(nodes, edges)
 g.print_dep_nodes()
+g.print_parents()
