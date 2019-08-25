ea718722e1f50d521af57d4d836e512496288c29
Anna <anna@WALTERLAB-AnnaM.local>
2019-08-25 12:10:34 -0600

    created 100 dataframes

--- main.py
+++ main.py
@@ -9,15 +9,17 @@ Created on Sat Aug 24, 2019
 from DAG import DAG
 import copy as c
 import pandas as pd
+import random as r
 
 nodes = 5
-nodes = 20
-edges = 20
+edges = 5
 
 
 g = DAG(nodes, edges)
 #g.print_dep_nodes()
 #g.print_parents()
+
+
 list_data=[]
 for i in range(1000):
     list_data.append(c.deepcopy(g.return_node_value_dict()))
@@ -28,5 +30,46 @@ for d in list_data:
     for k, v in d.items():  # d.items() in Python 3+
         data.setdefault(k, []).append(v)
 
-print(pd.DataFrame.from_dict(data))
-#print(data)
+df=pd.DataFrame.from_dict(data)
+#Select a random target
+nodes=sorted(g.node_dict.keys())
+target=r.randint(0, len(nodes))
+df=df.rename(columns={target: "Target"})
+
+
+
+
+export_csv = df.to_csv(r'first_rand_df.csv', index = None, header=True)
+
+for j in range(100):
+    print(j)
+    list_data=[]
+    for i in range(2):
+        list_data.append(c.deepcopy(g.return_node_value_dict()))
+#print(list_data)
+
+    data = {}
+    for d in list_data:
+        for k, v in d.items():  # d.items() in Python 3+
+            data.setdefault(k, []).append(v)
+
+    df=pd.DataFrame.from_dict(data)
+    #Select a random target
+    nodes=sorted(g.node_dict.keys())
+    target=r.randint(0, len(nodes))
+    df=df.rename(columns={target: "Target"})
+    print("Here is a dataframe for value %i" %j)
+    print(df)
+
+    export_csv = df.to_csv(r'rand_df'+str(j)+'.csv', index = None, header=True)
+
+    
+
+    
+
+
+
+
+
+#
+#
